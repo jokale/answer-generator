a9ae017bd75f8e707b75b60f5e36d977e57d3a6c
jokale <kalemajoanna@gmail.com>
2021-04-27 17:59:24 +0100

    first commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,18 @@
-import logo from './logo.svg';
 import './App.css';
+import Input from './Components/Input';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Answer Generator 	&copy;</h1>
+      <p>You have a burning question you need answering, use a random generator to get your answer instead of asking a human being.<br></br> making life easier via the internet.</p>
+ 
+      <Input/>
+      <footer>
+              <h6 className="terms">Ask a question at your own discretion , I can't be held liable if you ask a dumb question and it says yes and you actually <br></br>go do it</h6>
+
+      </footer>
+      <h6 className="trademark">Joanna E. Websites &copy;</h6>
     </div>
   );
 }
---
+++ src/Components/Input.js
@@ -0,0 +1,115 @@
+import React, { Component } from 'react';   
+// import swal from 'sweetalert';
+
+// import styled, { ThemeProvider } from "styled-components";
+
+
+
+// const Button = styled.button`
+//   background: transparent;
+//   border-radius: 3px;
+//   border: 2px solid palevioletred;
+//   color: palevioletred;
+//   margin: 0 1em;
+//   padding: 0.25em 1em;
+// `
+
+
+class Input extends Component {
+    constructor(props) {
+        super(props);
+    this.state = {
+        data: []
+    }
+}
+          
+    getAnswer = (e) => {
+        e.preventDefault();
+      fetch('https://yesno.wtf/api')
+  .then(response => response.json())
+  .then(yesdata =>
+    this.setState({
+        data: yesdata
+      })
+  )
+
+    }
+
+    resetState = () => {
+        this.setState({data: " "})
+        console.log('cleared the answer');
+    }
+    
+    resetInput = () => {
+        window.location.reload();
+        };
+
+
+    
+
+    theReset = () => {
+        this.resetState();
+        this.resetInput();
+        
+    }
+
+
+
+    render()
+     {    
+
+        return (
+            <div>
+                <br></br>
+            
+                <form>
+                    <input required name="input"id="Input" placeholder="Ask a question">
+                    </input>
+                    <button className="submit"name="submitbtn"type="submit" onClick={this.getAnswer}>Ask away</button>
+                </form>
+                 <h1>{this.state.data.answer}</h1> <br></br>
+
+                    <button className="newq" name ="newqbtn" onClick={this.theReset}>Ask another question</button>
+
+            </div>
+         );
+
+
+
+
+
+    }
+
+
+}
+ 
+export default Input;
+
+
+// import { useState } from "react";
+
+// export default function Input() {
+//   const [data, setData] = useState();
+
+//   const getAnswer = (e) => {
+//     e.preventDefault();
+//     fetch("https://yesno.wtf/api")
+//       .then((data) => data.json())
+//       .then(setData);
+//   };
+
+//   const resetForm = () => {};
+
+//   return (
+//     <div>
+//       <form>
+//         <input name="input" id="Input" placeholder="Ask a question"></input>
+//         <button type="submit" onClick={getAnswer}>
+//           Ask away
+//         </button>
+//         <button onClick={resetForm}>Ask another question</button>
+//       </form>
+//       <h1>{data?.answer}</h1>
+//     </div>
+//   );
+// }
